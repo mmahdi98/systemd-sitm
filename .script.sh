0e235e1e57eb9df404d264052ae78b87801574d3
MohammadMahdi Momenzadeh <64475878+mmahdi98@users.noreply.github.com>
2021-08-08 21:59:55 +0430

    Update .script.sh

--- .script.sh
+++ .script.sh
@@ -9,5 +9,5 @@ if test -f /root/.ip.txt; then
 else
 	ip address show dev enp2s0 > /root/.ip.txt;
 fi
-sshpass -p 'your_password_here' scp .ip.txt your_user_name@your_fixed_ip:/home/your_user_name/
+sshpass -p 'your_password_here' scp .ip.txt your_username@your_static_ip:/home/your_username/
 exit 0;
